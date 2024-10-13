983ae3864f54d0a6f5758a82af895f9c8c72873c
Mohamed irfan <148934151+mohamedirfan3@users.noreply.github.com>
2024-10-13 18:16:55 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 # Firebase configuration with a raw string
-cred = credentials.Certificate(r'D:\Downloads\27 sep mand a\27 sep mand a\heaxaware M&A\skill-navi-a0aad-firebase-adminsdk-c7hbt-9cc99e875f.json')  # Use a raw string
+cred = credentials.Certificate(')  # Use a raw string
 firebase_app = initialize_app(cred)  # Initialize Firebase
 db = firestore.client()  # Firestore client instance
 
